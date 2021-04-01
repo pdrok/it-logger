44a91f11aa13a4f22d8f9a9c1e76fd84a5a6594b
Pedro Lopez <plopez@poweranalytics.com>
2021-04-01 17:42:37 -0400

    Add, Delete, Update & Search Logs

--- src/components/logs/EditLogModal.js
+++ src/components/logs/EditLogModal.js
@@ -1,16 +1,34 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
 import M from "materialize-css/dist/js/materialize.min.js";
-const EditLogModal = () => {
+import { updateLog } from "../../actions/logActions";
+
+const EditLogModal = ({ current, updateLog }) => {
   const [message, setMessage] = useState("");
   const [attention, setAttention] = useState(false);
   const [tech, setTech] = useState("");
 
-  const onsubmit = () => {
+  useEffect(() => {
+    if (current) {
+      setMessage(current.message);
+      setAttention(current.attention);
+      setTech(current.tech);
+    }
+  }, [current]);
+  const onSubmit = () => {
     if (message === "" || tech === "") {
       M.toast({ html: "Please enter a message and tech" });
     } else {
-      console.log(message, tech, attention);
-
+      const updLog = {
+        id: current.id,
+        message,
+        attention,
+        tech,
+        date: new Date(),
+      };
+      updateLog(updLog);
+      M.toast({ html: `Log updated by ${tech}` });
       // Clear Fields
       setMessage("");
       setTech("");
@@ -29,9 +47,6 @@ const EditLogModal = () => {
               value={message}
               onChange={(e) => setMessage(e.target.value)}
             />
-            <label htmlFor='message' className='active'>
-              Log Message
-            </label>
           </div>
         </div>
         <div className='row'>
@@ -72,7 +87,7 @@ const EditLogModal = () => {
       <div className='modal-footer'>
         <a
           href='#!'
-          onClick={onsubmit}
+          onClick={onSubmit}
           className='modal-close waves-effect blue waves-light btn'
         >
           Enter
@@ -86,4 +101,12 @@ const modalStyle = {
   width: "75%",
   height: "75%",
 };
-export default EditLogModal;
+
+EditLogModal.propTypes = {
+  current: PropTypes.object,
+  updateLog: PropTypes.func.isRequired,
+};
+const mapStateToProps = (state) => ({
+  current: state.log.current,
+});
+export default connect(mapStateToProps, { updateLog })(EditLogModal);
--- src/reducers/logReducer.js
+++ src/reducers/logReducer.js
@@ -1,4 +1,14 @@
-import { GET_LOGS, SET_LOADING, LOGS_ERROR } from "../actions/type";
+import {
+  GET_LOGS,
+  SET_LOADING,
+  LOGS_ERROR,
+  ADD_LOG,
+  DELETE_LOG,
+  UPDATE_LOG,
+  SET_CURRENT,
+  CLEAR_CURRENT,
+  SEARCH_LOGS,
+} from "../actions/type";
 
 const initialState = {
   logs: null,
@@ -6,6 +16,8 @@ const initialState = {
   loading: false,
   error: null,
 };
+
+// eslint-disable-next-line
 export default (state = initialState, action) => {
   switch (action.type) {
     case GET_LOGS:
@@ -13,6 +25,40 @@ export default (state = initialState, action) => {
         ...state,
         logs: action.payload,
         loading: false,
+      };
+    case ADD_LOG:
+      return {
+        ...state,
+        logs: [...state.logs, action.payload],
+        loading: false,
+      };
+    case DELETE_LOG:
+      return {
+        ...state,
+        logs: state.logs.filter((log) => log.id !== action.payload),
+        loading: false,
+      };
+    case UPDATE_LOG:
+      return {
+        ...state,
+        logs: state.logs.map((log) =>
+          log.id === action.payload.id ? action.payload : log
+        ),
+      };
+    case SEARCH_LOGS:
+      return {
+        ...state,
+        logs: action.payload,
+      };
+    case SET_CURRENT:
+      return {
+        ...state,
+        current: action.payload,
+      };
+    case CLEAR_CURRENT:
+      return {
+        ...state,
+        current: null,
       };
     case SET_LOADING:
       return {
